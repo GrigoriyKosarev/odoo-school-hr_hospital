f95cb4db61190ae9434acab445549f2f0254cae7
Grigoriy <grigoriykosarev@gmail.com>
2022-10-29 07:02:00 -0700

    first edit

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
+#
 {
     'name': "hr_hospital",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Module hospital. Data clients.""",
 
     'description': """
-        Long description of module's purpose
+        This module use for doctors and hospitals.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Grigoriy Kosarev",
+    'website': "http://www.grigoriy-kosarev.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -26,10 +26,8 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
     ],
 }
