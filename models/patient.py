29b45e14473799539fcbc7e3c87c0344ddbc3c58
Grigoriy <grigoriykosarev@gmail.com>
2022-11-07 12:36:28 -0800

    edit

--- models/patient.py
+++ models/patient.py
@@ -2,10 +2,13 @@ from odoo import models, fields
 
 class Patient(models.Model):
     _name = 'hr.hos.patient'
-    _description = 'Patient description'
+    _description = 'Patient'
 
     name = fields.Char()
     full_name = fields.Char()
+    # sex = fields.
+    birthday = fields.Date()
+    years = fields.Integer()
 
     active = fields.Boolean(
         default=True, )
